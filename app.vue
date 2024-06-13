f3030c1002d3606b7fa86763a80f92737a7b98f6
Alladin <melico.alladin@gmail.com>
2024-06-13 15:57:35 +0800

    initial commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLayout>
+    <v-app>
+      <v-main>
+        <v-sheet :elevation="3" :height="200" :width="200" rounded>
+          <h1>Hello</h1>
+        </v-sheet>
+      </v-main>
+    </v-app>
+  </NuxtLayout>
+</template>
